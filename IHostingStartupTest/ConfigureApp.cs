e065bc37b180faf574592853999eb387500831d6
Chris Walker <chris@chriswalker.soy>
2019-05-09 13:07:12 +0100

    Init

---
+++ IHostingStartupTest/ConfigureApp.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+
+namespace IHostingStartupTest
+{
+    public class ConfigureApp : IStartup
+    {
+        public ConfigureApp()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging();
+            return services.BuildServiceProvider();
+        }
+
+        private IConfiguration Configuration { get; set; }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            Console.WriteLine(Configuration.GetSection("Test").GetValue<string>("TestValue"));
+        }
+    }
+}
---
+++ IHostingStartupTest/ConfigureHost.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(IHostingStartupTest.ConfigureHost))]
+namespace IHostingStartupTest
+{
+    public class ConfigureHost : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder
+                .UseKestrel()
+                .UseStartup<ConfigureApp>();
+        }
+
+        public static void Main(string[] args)
+        {
+            new WebHostBuilder()
+                .Build()
+                .Run();
+        }
+    }
+}
